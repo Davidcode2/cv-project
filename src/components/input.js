4030ec62d85c015781d0821e8beccc00b24f616e
Jakob Lingel <jlingel68@gmail.com>
2023-03-05 21:25:44 +0100

    Input to functional component

--- src/components/input.js
+++ src/components/input.js
@@ -1,22 +1,18 @@
-import React, { Component } from 'react';
-export class Input extends Component {
-  editButtonString = "";
+import React  from 'react';
+export function Input(props) {
 
-  constructor(props) {
-    super(props);
-    this.state = { firstName: '', lastName: '', DateOfBirth: '', email: '' }
-  }
-
-  render() {
-    const { edit, label, value, onChange } = this.props;
-    let output = edit
-      ? <input className="ma-l-auto" onChange={onChange} value={value} id={label} />
-      : <div>{value}</div>
-    return (
-      <div className="flex-row ma-1">
-        <label htmlFor={label} className="mr-05 mr-r-auto">{label}:</label>
-        {output}
-      </div>
-    )
-  }
+  const { edit, label, value, onChange } = props;
+  let output = edit ? (
+    <input className="ma-l-auto" onChange={onChange} value={value} id={label} />
+  ) : (
+    <div>{value}</div>
+  );
+  return (
+    <div className="flex-row ma-1">
+      <label htmlFor={label} className="mr-05 mr-r-auto">
+        {label}:
+      </label>
+      {output}
+    </div>
+  );
 }
